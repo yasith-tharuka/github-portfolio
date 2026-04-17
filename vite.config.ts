bdeb0fbbac3213e028e749f15a87baca5a8a84f3
Yasith Tharuka <tharukayasith8@gmail.com>
2026-04-17 20:26:27 +0530

    initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/github-portfolio/',
   plugins: [react()],
+  build: {
+    outDir: 'dist',
+    sourcemap: false,
+  },
 })
